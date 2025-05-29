d611c6c34b27dbd8739cb7f9d9debfa35d379abf
farikkm <farikk.v.2@gmail.com>
2025-05-29 16:22:08 +0500

    UPDATE operation is added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createTodo } from "./helpers/createTodo";
+import { httpRequest } from "./helpers/httpRequest";
 import { getPosts } from "./requests/getPosts";
 import { postTodo } from "./requests/postTodo";
 
@@ -8,8 +9,14 @@ const appHtml = document.querySelector("#app");
 const formHtml = document.querySelector(".form");
 const formButton = document.querySelector(".form button");
 
-async function main() {  
-  // Get Todos
+const updatePostModal = document.getElementById("update-modal");
+const updatePostForm = document.querySelector("#update-modal form");
+const updatePostInput = document.querySelector("#update-modal input");
+const updatePostTextarea = document.querySelector("#update-modal textarea");
+const updatePostButton = document.querySelector("#update-modal button");
+
+async function main() {
+  // Get Todos;
   let posts =
     JSON.parse(localStorage.getItem("posts")) || (await getPosts()) || [];
 
@@ -32,13 +39,13 @@ async function main() {
 
     const title = form.get("title");
     const body = form.get("body");
-    
+
     formButton.disabled = true;
     const newPost = await postTodo(title, body);
     formButton.disabled = false;
-    
-    posts.unshift(newPost)
-    localStorage.setItem("posts", JSON.stringify(posts))
+
+    posts.unshift(newPost);
+    localStorage.setItem("posts", JSON.stringify(posts));
 
     postsHtml.insertAdjacentHTML(
       "afterbegin",
@@ -47,6 +54,70 @@ async function main() {
   };
 
   formButton.addEventListener("click", handlePostSubmit);
+
+  [...postsHtml.children].forEach((elem, index) => {
+    elem.addEventListener("click", (e) => {
+      updatePostModal.classList.remove("hidden");
+
+      updatePostModal.addEventListener("click", (e) => {
+        if (!updatePostForm.contains(e.target)) {
+          document.body.classList.remove("_lock");
+          updatePostModal.classList.add("hidden");
+        }
+      });
+
+      const clickedPost = posts[index];
+
+      updatePostInput.value = clickedPost.title;
+      updatePostTextarea.value = clickedPost.body;
+
+      const updatedPost = {
+        title: clickedPost.title,
+        body: clickedPost.body,
+      };
+
+      updatePostButton.disabled = true;
+
+      updatePostInput.addEventListener("input", (e) => {
+        updatedPost.title = e.target.value;
+        updatePostButton.disabled = false;
+      });
+
+      updatePostTextarea.addEventListener("input", (e) => {
+        updatedPost.body = e.target.value;
+        updatePostButton.disabled = false;
+      });
+
+      updatePostForm.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const { data } = await httpRequest(
+          import.meta.env.VITE_API_URL + `/posts/1`,
+          "PUT",
+          {
+            "Content-Type": "application/json",
+          },
+          updatedPost
+        );
+
+        if (data) {
+          updatePostModal.classList.add("hidden");
+        }
+
+        posts[index].title = data.title;
+        posts[index].body = data.body;
+
+        elem.querySelector("h3").textContent = data.title;
+        elem.querySelector("p").textContent = data.body;
+
+        localStorage.setItem("posts", JSON.stringify(posts))
+      });
+
+      document.body.classList.add("_lock");
+    });
+  });
 }
+
+// Update Post
 
 window.addEventListener("DOMContentLoaded", main);
